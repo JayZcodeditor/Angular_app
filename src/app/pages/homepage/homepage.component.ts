a3ab5471e96e6b345622a4004b30244ed102e9bf
jthamma <jthammakit2546@gmail.com>
2024-11-21 00:30:33 +0700

    เพิ่มหน้า login และ homepage เพื่อข้ามกันไปมา

--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router'; // Import Router
 
 @Component({
+  standalone: true,
   selector: 'app-homepage',
-  imports: [],
   templateUrl: './homepage.component.html',
-  styleUrl: './homepage.component.css'
+  styleUrls: ['./homepage.component.css'],
 })
 export class HomepageComponent {
+  constructor(private router: Router) {} // Inject Router
 
+  // ฟังก์ชันเมื่อคลิกปุ่ม Logout
+  onLogout() {
+    this.router.navigate(['/login']); // นำทางกลับไปยังหน้า Login
+  }
 }
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router'; // Import Router
 import { FormsModule } from '@angular/forms';
 
 @Component({
@@ -12,8 +13,13 @@ export class LoginComponent {
   username: string = ''; // ตัวแปรผูกกับ ngModel
   password: string = ''; // ตัวแปรผูกกับ ngModel
 
+  constructor(private router: Router) {} // Inject Router
+
+  // ฟังก์ชันเมื่อกด Submit
   onSubmit() {
     console.log('Username:', this.username);
     console.log('Password:', this.password);
+    // นำทางไปยังหน้า Homepage
+    this.router.navigate(['/homepage']);
   }
 }
